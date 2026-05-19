803dd1829260a8451a3551cf2d07e199ab2af8aa
sulthan <sulthanshafeer713@gmail.com>
2026-05-19 22:03:37 +0530

    feat: live project activation complete with real firebase keys

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,12 +2,13 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB" + "...", // The user will need to add their actual key here
+    apiKey: "AIzaSyC7eCP8FrOrBZdNx88NmkN5FPpkasBSrko",
     authDomain: "fir-hosting-a57ec.firebaseapp.com",
     projectId: "fir-hosting-a57ec",
-    storageBucket: "fir-hosting-a57ec.appspot.com",
-    messagingSenderId: "...",
-    appId: "..."
+    storageBucket: "fir-hosting-a57ec.firebasestorage.app",
+    messagingSenderId: "756986493755",
+    appId: "1:756986493755:web:78087b5622eee71d94151e",
+    measurementId: "G-TBFYS7QYSM"
 };
 
 // Initialize Firebase
